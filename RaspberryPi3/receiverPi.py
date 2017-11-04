64fb84083aab60fe3e8dfca26c48b016bf191dc8
Juan González <jusgomen@gmail.com>
2017-11-04 14:30:08 -0500

    FFT on every axis

--- RaspberryPi3/receiverPi.py
+++ RaspberryPi3/receiverPi.py
@@ -55,21 +55,20 @@ def getFFT(BufferX, BufferY, BufferZ, Fs=200):
     zf = np.fft.fft(z,norm="ortho")
     zf = np.fft.fftshift(zf)
     
-	#Only returns positive spectrum side
+    #Only returns positive spectrum side
     return (f, xf[f>0], yf[f>0], zf[f>0])
 
 def sendFFT(freq, xfft, yfft, zfft):
-	n=len(freq)
-	try:
-		for i in range(n):
-			response=requests.get('http://track-mypower.tk/measurements/wind_turbine_frequencies/new?mag_x='+str(xfft[i])+'&mag_y='+str(yfft[i])+'&mag_z='+str(zfft[i])+'&freq='+str(freq[i])+'&mag='+str(xfft[i]),
-						auth=requests.auth.HTTPBasicAuth(
+    n=len(freq)
+    try:
+        for i in range(n):
+            response=requests.get('http://track-mypower.tk/measurements/wind_turbine_frequencies/new?mag_x='+str(xfft[i])+'&mag_y='+str(yfft[i])+'&mag_z='+str(zfft[i])+'&freq='+str(freq[i])+'&mag='+str(xfft[i]),
+            auth=requests.auth.HTTPBasicAuth(
 						  'admin',
 						  'uninorte'))	
-	except:
-		pass
-	return None
-
+    except:
+        pass
+        return None
 
 #No warnings available
 GPIO.setwarnings(False)
@@ -79,7 +78,6 @@ pipes = [[0xe7, 0xd3, 0xf0, 0x35, 0xff], [0xe7, 0xd3, 0xf0, 0x35, 0xc2]]
 
 radio = NRF24(GPIO, spidev.SpiDev())
 radio.begin(1, 17)
-
 radio.setRetries(15,15)
 radio.setPALevel(NRF24.PA_MAX)
 radio.setAutoAck(True)
@@ -112,135 +110,133 @@ lenBufAcc=0
 
 while True:
     while radio.available():
-		#print("Radio available")
+	#print("Radio available")
         received = []
         radio.read(received, radio.getDynamicPayloadSize())
 
-		if fftAvailable:
-			#Gets FFT from x, y and z
-			(freq, xfft, yfft, zfft) = getFFT(BufferX, BufferY, BufferZ)
-			sendFFT(freq.tolist(), xfft.tolist(), yfft.tolist(), zfft.tolist())
-			fftAvailable=False
+        if fftAvailable:
+            #Gets FFT from x, y and z
+            (freq, xfft, yfft, zfft) = getFFT(BufferX, BufferY, BufferZ)
+            sendFFT(freq.tolist(), xfft.tolist(), yfft.tolist(), zfft.tolist())
+            fftAvailable=False
 
         if len(received)>0:
-            
-			string=""
+            string=""
             for n in received:
                 if (n>=32 and n<=126):
                     string += chr(n)
             ##print(string)
 
-			if len(string)>0:
-		        if string[0]=='r':
-		            ind1=string.find('r')
-		            ind2=string.find('r',ind1+1)
+            if len(string)>0:
+                if string[0]=='r':
+                    ind1=string.find('r')
+                    ind2=string.find('r',ind1+1)
 
-		            rpm=string[ind1+1:ind2]
-		            
-		            try:
-		                irpm=int(rpm)
-		                print("Speed = "+ rpm + " RPM")
-		                
-		                if irpm==0:
-		                    rpm="0.0"
-		               
-		                try:
-		                    response_speed=requests.get('http://track-mypower.tk/measurements/wind_turbine_speed/new?rpm='+rpm+'&created_at='+str(date.now()),
-		                    auth=requests.auth.HTTPBasicAuth(
-		                      'admin',
-		                      'uninorte'))
-		                    
-		                    internet=True    
-		                except:
-		                    internet=False
-		                    if len(sendBuffer3)>211680:
-		                        sendBuffer3=sendBuffer3[1:]
-		                        sendBuffer3.append(rpm)
-		                    else:
-		                        sendBuffer3.append(rpm)
-								sendBuffer4=sendBuffer3[:]
-								lenBufRPM=len(sendBuffer3)
-		                        
-		            except:
-		                #print("There was an error in communication with the RPM sensor.")
-		                pass
+                    rpm=string[ind1+1:ind2]
+                            
+                    try:
+                        irpm=int(rpm)
+                        print("Speed = "+ rpm + " RPM")
+                        
+                        if irpm==0:
+                            rpm="0.0"
+                               
+                        try:
+                            response_speed=requests.get('http://track-mypower.tk/measurements/wind_turbine_speed/new?rpm='+rpm+'&created_at='+str(date.now()),
+                            auth=requests.auth.HTTPBasicAuth(
+                                      'admin',
+                                      'uninorte'))
+                                    
+                            internet=True    
+                        except:
+                            internet=False
+                            if len(sendBuffer3)>211680:
+                                sendBuffer3=sendBuffer3[1:]
+                                sendBuffer3.append(rpm)
+                            else:
+                                sendBuffer3.append(rpm)
+                                sendBuffer4=sendBuffer3[:]
+                                lenBufRPM=len(sendBuffer3)
+                             
+                    except:
+                        #print("There was an error in communication with the RPM sensor.")
+                        pass
 		        
-			if string[0]=='X'and len(string)>27:
-				indX=string.find('X')
-				indY=string.find('Y')
-				indZ=string.find('Z')
-				indF=string.find('F')
+                if string[0]=='X'and len(string)>27:
+                    indX=string.find('X')
+                    indY=string.find('Y')
+                    indZ=string.find('Z')
+                    indF=string.find('F')
 
-				mx=string[indX+1:indX+4]
-				maxx=string[indX+4:indX+7]
-				minx=string[indX+7:indX+10]
+                    mx=string[indX+1:indX+4]
+                    maxx=string[indX+4:indX+7]
+                    minx=string[indX+7:indX+10]
 
-				my=string[indY+1:indY+4]
-				maxy=string[indY+4:indY+7]
-				miny=string[indY+7:indY+10]
+                    my=string[indY+1:indY+4]
+                    maxy=string[indY+4:indY+7]
+                    miny=string[indY+7:indY+10]
 
-				mz=string[indZ+1:indZ+4]
-				maxz=string[indZ+4:indZ+7]
-				minz=string[indZ+7:indZ+10]
+                    mz=string[indZ+1:indZ+4]
+                    maxz=string[indZ+4:indZ+7]
+                    minz=string[indZ+7:indZ+10]
 
-				values=[maxx,mx,minx,maxy,my,miny,maxz,mz,minz]
+                    values=[maxx,mx,minx,maxy,my,miny,maxz,mz,minz]
 
-				try:
-					ivalues= [int(x) for x in values]
+                    try:
+                        ivalues= [int(x) for x in values]
 
-					#sendG is a list with Acceleration in g, datatype:int
-					sendG= [ toGx(x) for x in ivalues[0:3] ]
-					sendG+=[ toGy(x) for x in ivalues[3:6] ]
-					sendG+=[ toGz(x) for x in ivalues[6:]  ]
+                        #sendG is a list with Acceleration in g, datatype:int
+                        sendG= [ toGx(x) for x in ivalues[0:3] ]
+                        sendG+=[ toGy(x) for x in ivalues[3:6] ]
+                        sendG+=[ toGz(x) for x in ivalues[6:]  ]
 
-					if savedMaxMin<=400:
-						#Appends new max and min values for each axis
-						savedMaxMin+=1
-						maxMinX+=[sendG[0], sendG[2]]
-						maxMinY+=[sendG[3], sendG[5]]
-						maxMinZ+=[sendG[6], sendG[8]]
-					else :
+                        if savedMaxMin<=400:
+                            #Appends new max and min values for each axis
+                            savedMaxMin+=1
+                            maxMinX+=[sendG[0], sendG[2]]
+                            maxMinY+=[sendG[3], sendG[5]]
+                            maxMinZ+=[sendG[6], sendG[8]]
+                        else :
 
-						fftAvailable=True
+                            fftAvailable=True
 
-						#Starts new Buffer with these max and min values as the first ones		
-						savedMaxMin=1
-						maxMinX=[sendG[0], sendG[2]]
-						maxMinY=[sendG[3], sendG[5]]
-						maxMinZ=[sendG[6], sendG[8]]
+                            #Starts new Buffer with these max and min values as the first ones		
+                            savedMaxMin=1
+                            maxMinX=[sendG[0], sendG[2]]
+                            maxMinY=[sendG[3], sendG[5]]
+                            maxMinZ=[sendG[6], sendG[8]]
 
-					#sendIter is a list with Acceleration in g, datatype:string
-					sendIter= [str(x) for x in sendG]
-					print(sendIter)
+                        #sendIter is a list with Acceleration in g, datatype:string
+                        sendIter= [str(x) for x in sendG]
+                        print(sendIter)
 
-
-					#GET aceleraciones a base de datos
-					try:
-						requests.get('http://track-mypower.tk/measurements/wind_turbine_vibration/new?m_ejex='+sendIter[0]+'&m_ejey='+sendIter[3]+'&m_ejez='+sendIter[6]+'&created_at='+str(date.now()),
-						auth=requests.auth.HTTPBasicAuth(
-						  'admin',
-						  'uninorte'))
-						requests.get('http://track-mypower.tk/measurements/wind_turbine_vibration/new?m_ejex='+sendIter[2]+'&m_ejey='+sendIter[5]+'&m_ejez='+sendIter[8]+'&created_at='+str(date.now()),
-						auth=requests.auth.HTTPBasicAuth(
-						  'admin',
-						  'uninorte'))
-						internet=True
-					except:
-						internet=False
-						if len(sendBuffer1)>211680:
-							sendBuffer1=sendBuffer1[1:]
-							sendBuffer1.append(sendIter)
-						else:
-							sendBuffer1.append(sendIter)
-							sendBuffer1.append(sendIter)
-							sendBuffer2=sendBuffer1[:]
-							lenBufAcc=len(sendBuffer1)
-
+                        #GET aceleraciones a base de datos
+                        try:
+                            requests.get('http://track-mypower.tk/measurements/wind_turbine_vibration/new?m_ejex='+sendIter[0]+'&m_ejey='+sendIter[3]+'&m_ejez='+sendIter[6]+'&created_at='+str(date.now()),
+                            auth=requests.auth.HTTPBasicAuth(
+                              'admin',
+                              'uninorte'))
+                            requests.get('http://track-mypower.tk/measurements/wind_turbine_vibration/new?m_ejex='+sendIter[2]+'&m_ejey='+sendIter[5]+'&m_ejez='+sendIter[8]+'&created_at='+str(date.now()),
+                            auth=requests.auth.HTTPBasicAuth(
+                              'admin',
+                              'uninorte'))
+                            internet=True
+                        except:
+                            internet=False
+                            if len(sendBuffer1)>211680:
+                                    sendBuffer1=sendBuffer1[1:]
+                                    sendBuffer1.append(sendIter)
+                            else:
+                                    sendBuffer1.append(sendIter)
+                                    sendBuffer1.append(sendIter)
+                                    sendBuffer2=sendBuffer1[:]
+                                    lenBufAcc=len(sendBuffer1)
 
 
-				except:
-					#print("There was an error in communication with Accelerometer")
-					pass
+
+                    except:
+                        #print("There was an error in communication with Accelerometer")
+                        pass
                 
     #print("Radio unavailable")
     #time.sleep(0.01)
@@ -296,6 +292,3 @@ while True:
             continueB=True
 
     time.sleep(0.01)
-	if lenBufFFT>75:
-		(x_fft, y_fft, z_fft, f)=getFFT()
-    
